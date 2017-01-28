df9129c0bd637ebdf2e0fa9487bcc250d0c1b5cc
SW Test Academy <swtestacademy@gmail.com>
2017-01-29 00:52:47 +0300

    New files added

--- tests/acceptance/kariyer-main-page-test.js
+++ tests/acceptance/kariyer-main-page-test.js
@@ -5,6 +5,8 @@ moduleForAcceptance('Acceptance | kariyer main page');
 
 test('visiting /kariyer-main-page', function(assert) {
   visit('http://www.kariyer.net/');
+  
+   percySnapshot('Empty todo list1');
 
   andThen(function() {
     assert.equal(currentURL(), 'http://www.kariyer.net/');
